e807328d5d8c11ee8ec7acb0581065201afa7e42
Obanijesu <obanijesujohn45@gmail.com>
2022-11-17 08:53:26 -0600

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Quiz from "./Quiz";
+
+function App() {
+    return ( <
+        div >
+        <
+        Quiz > < /Quiz>{" "} <
+        /div>
+    );
+}
+
+export default App;
+
+const rootElement = document.getElementById("root");
+ReactDOM.render( < App > < /App>, rootElement);
